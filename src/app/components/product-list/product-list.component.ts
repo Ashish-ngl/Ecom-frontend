d6edad3af54f2ac136257349671a1ed374aa97e8
Ashish-ngl <ashish.raj@argusoft.in>
2024-10-04 11:55:56 +0530

    Show product list

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CurrencyPipe,CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+  
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+}
